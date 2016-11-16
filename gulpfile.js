c2f5c3707a8ea60d0763e6930d855ca4fa34eddd
Justin Harwood <justin@Justins-MacBook-Pro.local>
2016-11-16 15:24:12 -0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function(){
+  return gulp.src('*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('styles'))
+});
+
+gulp.task('autoprefix-then-minify', function(){
+  return gulp.src('styles/*.css')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions', '> 0%','Firefox ESR'],
+        cascade: false
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('styles'))
+});
+
+
+gulp.task('watch', function() {
+	gulp.watch('*.scss', ['sass']);
+	gulp.watch('styles/*.css', ['autoprefix-then-minify']);
+
+});
