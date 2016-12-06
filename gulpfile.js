8e62ed1557de460d4d3e9b41e9a569c9680887a3
justin <harwoodjp@g.cofc.edu>
2016-12-05 20:11:53 -0500

    improved

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,23 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
-  return gulp.src('*.scss')
+  return gulp.src('src/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('styles'))
+    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('autoprefix-then-minify', function(){
-  return gulp.src('styles/*.css')
+  return gulp.src('dist/*.css')
     .pipe(autoprefixer({
         browsers: ['last 2 versions', '> 0%','Firefox ESR'],
         cascade: false
     }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('styles'))
+    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('watch', function() {
-	gulp.watch('*.scss', ['sass']);
-	gulp.watch('styles/*.css', ['autoprefix-then-minify']);
+	gulp.watch('src/*.scss', ['sass']);
+	gulp.watch('dist/*.css', ['autoprefix-then-minify']);
 
 });
