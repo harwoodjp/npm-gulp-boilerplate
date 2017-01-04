8360f10bf7af432c388516a5e69436a9cd782497
Justin <harwoodjp@g.cofc.edu>
2017-01-04 14:54:30 -0500

    css and js moved to static folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,23 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
-  return gulp.src('src/*.scss')
+  return gulp.src('static/css/src/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('static/css/dist'))
 });
 
 gulp.task('autoprefix-then-minify', function(){
-  return gulp.src('dist/*.css')
+  return gulp.src('static/css/dist/*.css')
     .pipe(autoprefixer({
         browsers: ['last 2 versions', '> 0%','Firefox ESR'],
         cascade: false
     }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('static/css/dist'))
 });
 
 gulp.task('watch', function() {
-	gulp.watch('src/*.scss', ['sass']);
-	gulp.watch('dist/*.css', ['autoprefix-then-minify']);
+	gulp.watch('static/css/src/*.scss', ['sass']);
+	gulp.watch('static/css/dist/*.css', ['autoprefix-then-minify']);
 
 });
